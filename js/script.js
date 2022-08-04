a0111138925bb53fe16d79f5d092733691c80bde
santosds <santosjr4ds@gmail.com>
2022-08-03 23:31:54 -0300

    Primeira equação

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-var resposta = confirm ("Deseja excluir");
+//var resposta = confirm ("Deseja excluir");
 
-console.log(resposta);
+/*console.log(resposta);*/
+
+var a = 10;
+var b = 33;
+
+function somar(a, b){
+  return a + b;
+}
+
+console.log(soma(2, 5));
